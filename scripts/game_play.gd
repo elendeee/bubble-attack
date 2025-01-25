4c326c8e672fdb7160d9e67cbf104006b582d109
Elen Debina <112352262+elendeee@users.noreply.github.com>
2025-01-25 19:10:30 +0700

    init

--- scripts/game_play.gd
+++ scripts/game_play.gd
@@ -8,9 +8,14 @@ func _ready():
 	player = get_tree().get_first_node_in_group("player")
 	assert(player!=null)
 	player.global_position = player_spawn_pos.global_position
-
+	player.bubble_shot.connect(_on_player_bubble_shot)
+	
 func _process(_delta):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
 	elif Input.is_action_just_pressed("reset"):
 		get_tree().reload_current_scene()
+		
+func _on_player_bubble_shot(bubble_scene, location):
+		var bubble = bubble_scene.instantiate()
+		bubble.global_positian = location
