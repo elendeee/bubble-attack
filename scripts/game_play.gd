bc6ebf205a6df75dc91309095496ee097a353c95
Siska Putri <91827161+Siputsss@users.noreply.github.com>
2025-01-26 10:26:08 +0700

    sound effect

--- scripts/game_play.gd
+++ scripts/game_play.gd
@@ -8,6 +8,11 @@ extends Control
 @onready var enemy_container = $EnemyContainer
 @onready var hud = $UILayer/HUD
 @onready var gos = $UILayer/GameOverScreen
+@onready var pb = $ParallaxBackground
+
+@onready var bubble_sound = $SFX/BubbleSound
+@onready var pop_sound = $SFX/PopSound
+@onready var lose_sound = $SFX/LoseSound
 
 var player = null
 var score := 0:
@@ -15,6 +20,8 @@ var score := 0:
 		score = value
 		hud.score = score
 var high_score
+
+var scroll_speed = 100
 
 func _ready():
 	var save_file = FileAccess.open("user://save.data", FileAccess.READ)
@@ -45,25 +52,33 @@ func _process(delta):
 		timer.wait_time -= delta * 0.005
 	elif timer.wait_time < 0.5:
 		timer.wait_time = 0.5
-	print(timer.wait_time)
+	
+	pb.scroll_offset.x += delta * scroll_speed
 		
 func _on_player_bubble_shot(bubble_scene, location):
 		var bubble = bubble_scene.instantiate()
 		bubble.global_position = location
 		bubble_container.add_child(bubble)
+		bubble_sound.play()
 
 func _on_enemy_spawn_timer_timeout():
 	var e = enemy_scenes.pick_random().instantiate()
 	e.global_position = Vector2(randf_range(1300, 1700), randf_range(90, 600))
 	e.killed.connect(_on_enemy_killed)
+	e.hit.connect(_on_enemy_hit)
 	enemy_container.add_child(e)
 	
 func _on_enemy_killed(points):
+	pop_sound.play()
 	score += points
 	if score > high_score:
 		high_score = score
 
+func _on_enemy_hit():
+	pop_sound.play()
+
 func _on_player_killed():
+	lose_sound.play()
 	gos.set_score(score)
 	gos.set_high_score(high_score)
 	save_game()
